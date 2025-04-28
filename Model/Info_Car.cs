7dbee19cf7191af07b997d732a26e730396477e7
kingseo0715 <kingseo0715@gmail.com>
2025-04-28 12:04:04 +0900

    Add files via upload

---
+++ Model/Info_Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInfoClient.Model
+{
+    public class Info_Car : INotifyPropertyChanged
+    {
+        private int _oil;
+        public int Oil
+        {
+            get { return _oil; }
+            set
+            {
+                if (_oil != value)
+                {
+                    _oil = value;
+                    OnPropertyChanged(nameof(Oil));  // 바뀔 때 UI에 알림
+                }
+            }
+        }
+
+        private int _engine;
+        public int Engine
+        {
+            get { return _engine; }
+            set
+            {
+                if (_engine != value)
+                {
+                    _engine = value;
+                    OnPropertyChanged(nameof(_engine));  // 바뀔 때 UI에 알림
+                }
+            }
+        }
+
+        //livechar 데이터 바인딩을 위한 구현
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Model/Member.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInfoClient.Model
+{
+    public class Member 
+    {
+        private int _Num;
+        public int Num
+        {
+            get { return _Num; }
+            set { _Num = value; }
+        }
+        private string _type = string.Empty;
+        public string type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+        private Byte[] _file;
+        public Byte[] file
+        {
+            get { return _file; }
+            set { _file = value; }
+        }
+        private string _user_name = string.Empty;
+        public string user_name
+        {
+            get { return _user_name; }
+            set { _user_name = value; }
+        }
+
+        
+    }
+}
